fcbd28cbfc1513e12661220d736b9a865d1c9650
fabiano de sousa lima <fabianosl@ufrrj.br>
2025-02-11 20:40:24 -0300

    init

---
+++ home-finance-api/src/test/java/fabiano/homefinanceapi/HomeFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fabiano.homefinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
