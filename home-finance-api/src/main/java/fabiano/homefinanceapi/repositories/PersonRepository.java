59d150dd878319911c085f1f996774be4b88c573
fabiano de sousa lima <fabianosl@ufrrj.br>
2025-02-11 21:22:33 -0300

    feat(repositories): add transaction and person repositories

---
+++ home-finance-api/src/main/java/fabiano/homefinanceapi/repositories/PersonRepository.java
@@ -0,0 +1,12 @@
+package fabiano.homefinanceapi.repositories;
+
+import fabiano.homefinanceapi.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> all();
+}
---
+++ home-finance-api/src/main/java/fabiano/homefinanceapi/repositories/TransactionRepository.java
@@ -0,0 +1,12 @@
+package fabiano.homefinanceapi.repositories;
+
+import fabiano.homefinanceapi.entities.Transaction;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository  extends CrudRepository<Transaction, Long> {
+    List<Transaction> all();
+}
