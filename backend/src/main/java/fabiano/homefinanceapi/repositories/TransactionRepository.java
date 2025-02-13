64ff57d3b1d7fa4be19504eacbc41912ed6b4f56
fabiano de sousa lima <fabianosl@ufrrj.br>
2025-02-13 00:39:06 -0300

    fix(transaction): using JpaRepository

--- backend/src/main/java/fabiano/homefinanceapi/repositories/TransactionRepository.java
+++ backend/src/main/java/fabiano/homefinanceapi/repositories/TransactionRepository.java
@@ -1,9 +1,10 @@
 package fabiano.homefinanceapi.repositories;
 
 import fabiano.homefinanceapi.entities.Transaction;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
